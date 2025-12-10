45fde4b30abbff537a078fcffdd40c4cc7d11fb7
nava1975 <87694672+nava1975@users.noreply.github.com>
2025-12-10 06:09:10 +0200

    Remove weather data loading from AppComponent
    Removed weather data loading logic from AppComponent.

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService, WeatherForecast } from './services/api.service';
+import { ApiService } from './services/api.service';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 
@@ -10,29 +10,13 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'Client';
-  weatherData: WeatherForecast[] = [];
+  
   loading = false;
   error = '';
 
   constructor(private apiService: ApiService, public authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    this.loadWeatherData();
-  }
-
-  loadWeatherData(): void {
-    this.loading = true;
-    this.error = '';
-    this.apiService.getWeatherForecast().subscribe({
-      next: (data) => {
-        this.weatherData = data;
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = 'Failed to load weather data: ' + err.message;
-        this.loading = false;
-      }
-    });
   }
 
   logout() {
